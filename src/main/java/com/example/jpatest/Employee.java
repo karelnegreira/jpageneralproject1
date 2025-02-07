57afbcce0ed9a7f7b468aef8337c1b20488bb6f9
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-02-08 01:51:31 +1200

    first commit Employee entity created

---
+++ src/main/java/com/example/jpatest/Employee.java
@@ -0,0 +1,52 @@
+package com.example.jpatest;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import org.springframework.data.annotation.Id;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstname;
+    private String lastname;
+    //jpa no args constructor
+    public Employee() {}
+
+    public Employee(String lastname, String firstname, Long id) {
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Employee(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+}
